5e50652b3d1256986f243d38d6aa9d2171a1c6d0
zacharyenright <70986414+zacharyenright@users.noreply.github.com>
2022-10-03 09:18:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import random as rand
+from turtle import back
 import numpy as np
 import pygame
+pygame.init()
+
+# Main Window Variables
+bgColor = (255, 255, 255)
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Sudoku')
+win.fill(bgColor)
+original_grid_element_color = (255, 255, 255)
+dif = 500 / 9
+
+# Fonts
+fontSans = pygame.font.SysFont("comicsans", 40)
+fontSansSmall = pygame.font.SysFont("comicsans", 20)
 
 # Blank grid
 blankGrid = [
@@ -24,42 +38,72 @@ def randBoard(grid):
     # Declare global variable
     global randGrid
 
+    nonZeroDecimals = 0
     # Iterate through every index of the grid
     for i in range (len(grid)):
         for j in range (len(grid[i])):
             grid[i][j] = rand.randint(0,9)
 
+            '''
+            # Break if too many non-zero digits appear
+            if(grid[i][j] != 0):
+                nonZeroDecimals += 1
+                if(nonZeroDecimals > 30):
+                    break
+            '''
+
     # Set global variable randGrid equal to local variable grid
     randGrid = grid
     printBoard(grid)
 
+def checkHorizontal(board, x):
+    totSum = 0
+    for i in range(9):
+            totSum += board[x][i]
+            if(i == 8 and totSum == 45):
+                return True
+            elif(i == 8 and totSum != 45):
+                return False
+
+
+# Basic algorithim
+def backAlg(board):    
+    # Move through every cell and change each empty cell to '?'
+    for i in range(9):
+        for j in range(9):
+            if (board[i][j] == 0):
+                if(checkHorizontal(board, 0)):
+                    print("Unique")
+                else:
+                    print("Not unique")
+
 # Main function
 def main(grid):
-    # Main Window Variables
-    bgColor = (0, 0, 0)
-    win = pygame.display.set_mode((550, 550))
-    pygame.display.set_caption('Sudoku')
-    win.fill(bgColor)
-    original_grid_element_color = (255, 255, 255)
+   # Fill background color of non-zero cells
+    for i in range (len(grid)):
+        for j in range (len(grid)):
+            if grid[i][j]!= 0:
+                # Fill blue color in already numbered grid
+                if(int(grid[i][j]) > 0): 
+                    pygame.draw.rect(win, (0, 153, 153), (i * dif, j * dif, dif + 1, dif + 1))
+                
+                # Display board
+                text1 = fontSans.render(str(grid[i][j]), 1, (0, 0, 0))
+                win.blit(text1, (i * dif + 15, j * dif))
 
-    # Draw Grid Lines
-    for i in range(0,10):
-        if(i%3 == 0):
-            #drwaing the block line (vertical)
-            pygame.draw.line(win, (255, 255, 255), (50 + 50*i, 50), (50 + 50*i ,500 ), 4 )
-            #(Horizontal)
-            pygame.draw.line(win, (255, 255, 255), (50, 50 + 50*i), (500, 50 + 50*i), 4 )
-        #drwaing vertical line
-        pygame.draw.line(win, (166, 166, 166), (50 + 50*i, 50), (50 + 50*i ,500 ), 2 )
-        #drwaing horizental line
-        pygame.draw.line(win, (166, 166, 166), (50, 50 + 50*i), (500, 50 + 50*i), 2 )
-
+    # Draw lines horizontally and vertically to form grid          
+    for i in range(10):
+        if i % 3 == 0 :
+            thick = 7
+        else:
+            thick = 1
+        pygame.draw.line(win, (0, 0, 0), (0, i * dif), (500, i * dif), thick)
+        pygame.draw.line(win, (0, 0, 0), (i * dif, 0), (i * dif, 500), thick)            
+    
 
     # Update the display using flip(), and bool to keep game running.
     pygame.display.flip()
     running = True
-
-    pygame.draw.line(win, (255, 255, 255), (50 + 50*i, 50), (50 + 50*i ,500 ), 4 )
 
     # Keep window running until closed
     while running:
@@ -70,4 +114,6 @@ def main(grid):
                 running = False
 
 
-main(blankGrid)
+randBoard(blankGrid)
+backAlg(randGrid)
+main(randGrid)
